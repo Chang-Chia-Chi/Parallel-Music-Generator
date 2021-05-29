d944be4f9fb9019af612b548df47d2a8eeb6d65f
Chang-Chia-Chi <4759645@gmail.com>
2021-05-29 23:13:46 +0800

    Modify variables and add functions

--- training/markov.h
+++ training/markov.h
@@ -6,8 +6,14 @@
 #define NUM_CHORD 1728
 #define NUM_NOTE NUM_TONE * NUM_DURATION
 #define CHORD_BASE 99
-#define BUFFER_LEN 200000
+#define BUFFER_LEN 30000000
 #define NUM_THREADS 1024
+
+struct note_info {
+    int tone;
+    int dur;
+    int tune;
+};
 
 extern int* major_high;
 extern int* major_low;
@@ -16,23 +22,15 @@ extern int* minor_low;
 extern int* major_chord;
 extern int* minor_chord;
 
-extern int* cuda_majorHighTone;
-extern int* cuda_majorHighDur;
-extern int* cuda_majorLowTone;
-extern int* cuda_majorLowDur;
-extern int* cuda_minorHighTone;
-extern int* cuda_minorHighDur;
-extern int* cuda_minorLowTone;
-extern int* cuda_minorLowDur;
+extern note_info* majorHighBuff;
+extern note_info* majorLowBuff;
+extern note_info* minorHighBuff;
+extern note_info* minorLowBuff;
 
-extern int* device_majorHighTone;
-extern int* device_majorHighDur;
-extern int* device_majorLowTone;
-extern int* device_majorLowDur;
-extern int* device_minorHighTone;
-extern int* device_minorHighDur;
-extern int* device_minorLowTone;
-extern int* device_minorLowDur;
+extern note_info* device_majorHighBuff;
+extern note_info* device_majorHighBuff;
+extern note_info* device_majorLowBuff;
+extern note_info* device_majorLowBuff;
 
 extern int* device_majorHighNotes;
 extern int* device_majorLowNotes;
@@ -42,11 +40,13 @@ extern int* device_majorChords;
 extern int* device_minorChords;
 
 // cuda functions called by main
-void cuda_note_count(int* high_tone, int* hign_dur, int* low_tone, int* low_dur, int high_len, int low_len, int is_major, int tune);
+void cuda_note_count(note_info* high_buff, note_info* low_buff, int high_len, int low_len, int is_major);
+void buffer_copy(note_info* high_buff, note_info* low_buff, int high_len, int low_len, int is_major);
 void cuda_malloc();
 void cuda_stream_create();
 void cuda_to_host();
 void cuda_free();
+void cuda_note_count(int high_len, int low_len, int is_major);
 void cuda_stream_destroy();
 void cuda_stream_synch(int is_major);
 
